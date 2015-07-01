02adbafc17c9395178fd12481480ccfb520cba73
Andrew Dahms <adahms@redhat.com>
2015-07-01 23:48:08 +1000

    Migrated the API mappings reference to the Git repository.

---
+++ doc-Red_Hat_CloudForms_SOAP_API_to_REST_API_Mapping/en-US/files/pressgang_website.js
@@ -0,0 +1,7 @@
+pressgang_website_callback([
+	{"topicId":44416,"target":"Control_Web_Services","title":"Control Web Services","newSince":""},
+	{"topicId":44417,"target":"Insight_Web_Services","title":"Insight Web Services","newSince":""},
+	{"topicId":44418,"target":"Automate_Web_Services","title":"Automate Web Services","newSince":""},
+	{"topicId":42938,"target":"Author_Group","title":"Author Group","newSince":""},
+	{"topicId":44476,"target":"sect-Mappings","title":"Mappings","newSince":""},
+	{"topicId":44477,"target":"appe-Revision_History","title":"Revision History","newSince":""}], 23102, "Red Hat CloudForms", "Red Hat CloudForms SOAP API to REST API Mapping", "3.2");

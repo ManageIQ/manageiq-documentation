5c2239c6ea2c001e6640e7931d0cc03b37b09739
Andrew Dahms <adahms@redhat.com>
2015-07-01 20:36:51 +1000

    Migrated the ServiceNow guide to the Git repository.

---
+++ doc-Integration_with_ServiceNow/en-US/files/pressgang_website.js
@@ -0,0 +1,14 @@
+pressgang_website_callback([
+	{"topicId":43200,"target":"VM_Retirement","title":"VM Retirement","newSince":""},
+	{"topicId":43201,"target":"VM_Reconfiguration_VMware_Only","title":"VM Reconfiguration (VMware Only)","newSince":""},
+	{"topicId":30322,"target":"Abstract","title":"Abstract","newSince":""},
+	{"topicId":43192,"target":"chap-Overview","title":"Overview","newSince":""},
+	{"topicId":43193,"target":"chap-Configuring_ServiceNow_Connection","title":"Configuring ServiceNow Connection","newSince":""},
+	{"topicId":43194,"target":"chap-Managing_Record_Attributes","title":"Managing Record Attributes","newSince":""},
+	{"topicId":42938,"target":"Author_Group","title":"Author Group","newSince":""},
+	{"topicId":43195,"target":"Create_and_Update_patch_Record","title":"Create and Update (_patch) Record","newSince":""},
+	{"topicId":43196,"target":"Get_and_Delete_Record","title":"Get and Delete Record","newSince":""},
+	{"topicId":43197,"target":"Get_All_Records","title":"Get All Records","newSince":""},
+	{"topicId":43198,"target":"chap-Use_Cases","title":"Use Cases","newSince":""},
+	{"topicId":44414,"target":"appe-Revision_History","title":"Revision History","newSince":""},
+	{"topicId":43199,"target":"Provision_VM_from_Template","title":"Provision VM from Template","newSince":""}], 23064, "Red Hat CloudForms", "Integration with ServiceNow", "3.2");

5f18ed0e90699ec548c96427e37750848c0e5385
Andrew Dahms <adahms@redhat.com>
2015-11-23 20:02:56 +1000

    Added a script to automate compilation of in-appliance docs.

---
+++ pdfbuild.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# ##################################################
+# Created on: Mon, 23 Nov 2015
+# Created by: Andrew Dahms
+# 
+# Creates a PDF version of the guides in the core
+# documentation suite, and compiles them into a
+# TAR file.
+# ##################################################
+
+echo ========================================
+echo Appliance Docs Compiler
+echo ========================================
+
+echo 
+echo "Creating temporary directory..."
+echo 
+
+TMPDIR=cfme-`date +"%Y-%m-%d"`
+
+mkdir -p ${TMPDIR}
+
+sleep 0.5
+
+# Use ccutil to build PDF files for each of the core guides
+for book in doc-Defining_Policies_and_Profiles doc-General_Configuration doc-Managing_Infrastructure_and_Inventory doc-Managing_Providers doc-Methods_Available_for_Automation doc-Monitoring_Alerts_and_Reporting doc-Provisioning_Virtual_Machines_and_Hosts doc-Scripting_Actions_in_CloudForms
+do
+
+ echo "Generating PDF for" ${book}...
+ echo
+
+ cd ${book}
+
+ ccutil compile --format pdf --lang en-US > /dev/null 2>&1
+
+ mv build/tmp/en-US/pdf/* ../${TMPDIR}
+
+ ccutil clean > /dev/null 2>&1
+
+ cd ..
+
+done
+
+# Rename PDF files to use the naming scheme specified for the appliance
+echo "Renaming PDF files..."
+echo
+
+sleep 0.5
+
+mv *Defining_Policies_and_Profiles-en-US.pdf defining_policies_profiles.pdf
+mv *General_Configuration-en-US.pdf general_configuration.pdf
+mv *Managing_Infrastructure_and_Inventory-en-US.pdf infrastructure_inventory.pdf
+mv *Managing_Providers-en-US.pdf providers.pdf
+mv *Methods_Available_for_Automation-en-US.pdf methods_for_automation.pdf
+mv *Monitoring_Alerts_and_Reporting-en-US.pdf monitoring_alerts_reporting.pdf
+mv *Provisioning_Virtual_Machines_and_Hosts-en-US.pdf virtual_machines_hosts.pdf
+mv *Scripting_Actions_in_CloudForms-en-US.pdf scripting_actions.pdf
+
+# Archive PDF files
+echo
+echo Archiving PDF files...
+echo
+
+sleep 0.5
+
+tar cvf ${TMPDIR}.tar ${TMPDIR}/*
+
+echo
+echo "Successfully created archive!"
+echo
+
+echo "Cleaning up temporary directory..."
+echo
+
+sleep 0.5
+
+rm -rf ${TMPDIR} 
